dcb83ca1cf710ef168ec600d2a81c5718ab242d6
Aurtherle <113518431+Aurtherle@users.noreply.github.com>
2023-06-20 15:40:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,41 +21,41 @@ global.lApiKey = [ // if this API key get expired then please go to https://api.
 ]
 
 
-global.botname = "Queen Anya" 
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.footer = "© Queen Anya Bot"
-global.prefa = ['-'] // single prefix
+global.botname = "AURTHER" 
+global.author = "AURTHER"  
+global.packname = "AURTHER"  
+global.myweb = "AURTHER.COM" // You can put any link 
+global.waterMark = "AURTHER" // Your own watermark 
+global.footer = "AURTHER LEWIN"
+global.prefa = ['.'] // single prefix
 global.themeemoji = '🔥'
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['918811074852'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "AURTHER"   // Owner name //        
+global.ownernumber = ['212684151146'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : ki.m._.o" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.region = "KSA" // your country name
+global.state = "Riyadh" // your state name
+global.district = "ALKHALDIAH" // your district name
+global.timezone = "Asia/Riyadh" // search on google if you don't know the timezone of your country //
+global.instagramId = "ki.m._.o" // Your insta ID
+global.email = "hatg8004@gmail.com" // put your email here
 
 
 // Greeting messages
-global.greet1 = "Good morning ☀️" // after 3 AM
-global.greet2 = "Good afternoon 🏜️" // after 12 PM
-global.greet3 = "Good evening 🌆" // after 4 PM
-global.greet4 = "Good night 😴" // after 8:30 PM
+global.greet1 = "صباح الخير الصلاة قربت ☀️" // after 3 AM
+global.greet2 = "شمسوين صلاة الظهر قربت 🏜️" // after 12 PM
+global.greet3 = "صليتوا عصر ؟ 🌆" // after 4 PM
+global.greet4 = "هل صليتم المغرب والعشاء ؟ 😴" // after 8:30 PM
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','91881107****']  // Co - Owners
+global.premium = ['212684151146','917355******']  // available soon
+global.sudo = ['212684151146','91881107****']  // Co - Owners
 
 
 //--------------- Tip ----------------\\
